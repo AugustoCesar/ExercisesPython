ef4d8eee8523d911892c99057e1eb9afd8729ab3
AugustoCesar <augustocesar.udi@gmail.com>
2020-06-23 15:52:41 -0400

    Add files via upload

---
+++ listaEstruturaSequencial.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  9 10:44:01 2020
+
+@author: Augusto
+"""
+import math
+#1 Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+#print ("Alo Mundo")
+
+#2 Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número]. 
+# num = input("Type a number: ")
+# print("The number typed was ",num)
+
+#3 Faça um Programa que peça dois números e imprima a soma.
+# num_1 = float(input("Type a number: "))
+# num_2 = float(input("Now, type a second number: "))
+# answer = num_1 + num_2
+# print (num_1," + ", num_2, "= ", answer)
+
+# #4 Faça um Programa que peça as 4 notas bimestrais e mostre a média. 
+# nb_1 = float(input("Type the first bimester's note: "))
+# nb_2 = float(input("Type the second bimester's note: "))
+# nb_3 = float(input("Type the third bimester's note: "))
+# nb_4 = float(input("Type the fourth bimester note: "))
+
+# average = float((nb_1+nb_2+nb_3+nb_4)/4)
+
+# print("The average of notes was: ",average)
+
+# #5 Faça um Programa que converta metros para centímetros
+# m = float(input("Type the value in meters: "))
+# cm = float(m*100)
+# print(m, " meters equals %.1f centimeters!" % cm)
+
+# 6Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+# r = float(input("Type the value of radius: "))
+# area_circle = math.pi * (r**2)
+# print ("The area of the circle is: %.2f" % area_circle)
+
+# 7 Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
+# side_square = float(input("Type the value of the square's side: "))
+# double_area_square = math.pow(side_square, 2) * 2
+# print("The double of the square's area is: %.2f" % double_area_square)
+
+# 8 Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês. Calcule e mostre o total do seu salário no referido mês.
+# v_hour = float(input("How much do you earn per hour: "))
+# qtd = float(input("How many hours do you work per month: "))
+# salary = v_hour * qtd
+# print("Your salary is: %.2f" % salary)
+
+# 9 Faça um Programa que peça a temperatura em graus Farenheit, transforme e mostre a temperatura em graus Celsius. 
+# f = float(input("Type the temperature in Farenheit: "))
+# c = (5 * (f-32) / 9)
+# print ("%.2f " % f + " Farenheit equals %.2f "% c + " Celsius")
+
+# 10 Faça um Programa que peça a temperatura em graus Celsius, transforme e mostre em graus Farenheit. 
+# c = float(input("Type the temperature in Celsius: "))
+# f = (c*9/5) + 32
+# print ("%.2f " % c + " Celsius equals %.2f "% f + " Farenheit")
